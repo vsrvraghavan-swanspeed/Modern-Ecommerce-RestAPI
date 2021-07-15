c993bbeecd4ade2a9a68a53b34701c5ec030e2c2
vsrvraghavan-swanspeed <87170576+vsrvraghavan-swanspeed@users.noreply.github.com>
2021-07-15 13:07:08 +0530

    Update FileUtil.java
    added exception handler for copyFile method

--- src/main/java/com/nitsoft/util/FileUtil.java
+++ src/main/java/com/nitsoft/util/FileUtil.java
@@ -93,7 +93,7 @@ public class FileUtil {
             ic.close();
             oc.close();
         }catch(Exception e) {
-            System.out.println("Exception copyFile : " + e.getMessage);
+            System.out.println("Exception in copyFile ");
         }
     }
 
