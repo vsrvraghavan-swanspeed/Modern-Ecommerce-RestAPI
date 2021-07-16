66b39928ffb9e150c8ddc2f9a05576f4cf180d0c
vsrvraghavan-swanspeed <87170576+vsrvraghavan-swanspeed@users.noreply.github.com>
2021-07-16 11:39:39 +0530

    Update FileUtil.java

--- src/main/java/com/nitsoft/util/FileUtil.java
+++ src/main/java/com/nitsoft/util/FileUtil.java
@@ -87,13 +87,23 @@ public class FileUtil {
 
     public static void copyFile(String in, String out) throws Exception {
 
-            FileChannel ic = new FileInputStream(in).getChannel();   
-            FileChannel oc = new FileOutputStream(out).getChannel();
-            ic = new FileInputStream(in).getChannel();
-            oc = new FileOutputStream(out).getChannel();
-            oc.transferFrom(ic, 0, ic.size());                    
-            ic.close();
-            oc.close();
+            try(FileChannel ic = new FileInputStream(in).getChannel()){
+                try(FileChannel oc = new FileOutputStream(out).getChannel()){
+                    oc.transferFrom(ic, 0, ic.size());
+                }catch(Eception e){
+                    oc.close();
+                }                    
+            }catch(Exception e){
+                ic.close();
+            }finally{
+                ic.close();
+                oc.close();
+            }
+            //FileChannel ic = new FileInputStream(in).getChannel();   
+            //FileChannel oc = new FileOutputStream(out).getChannel();           
+            //oc.transferFrom(ic, 0, ic.size());                    
+            //ic.close();
+            //oc.close();
     }
 
     public static void copyFolder(String in, String out) throws Exception {
