622a9b32070cb77db5061a0d199d9db52f045fef
nirmalya-at-swanspeed <nirmalya.s@swan-speed.com>
2021-07-20 22:56:59 +0530

    - Again, modified JUnit tests with assertEquals method to satisfy Sonarcube (the order of actual/expected should be reversed)

--- src/test/java/com/nitsoft/util/test/StringUtilTest.java
+++ src/test/java/com/nitsoft/util/test/StringUtilTest.java
@@ -14,16 +14,16 @@ public class StringUtilTest {
     public void zeroPadIntWith1DigitTest() {
         String paddedString = StringUtil.zeroPadInt(5,1);
 
-        Assert.assertEquals (paddedString, 1);
-        Assert.assertEquals(paddedString,("5"));
+        Assert.assertEquals (1,   paddedString.length());
+        Assert.assertEquals ("5", paddedString);
     }
 
     @Test
     public void zeroPadIntWithFewerDigitsTest() {
         String paddedString = StringUtil.zeroPadInt(500,1);
 
-        Assert.assertEquals (paddedString.length(),3);
-        Assert.assertEquals (paddedString, ("500"));
+        Assert.assertEquals (3, paddedString.length());
+        Assert.assertEquals ("500",paddedString);
     }
 
 
@@ -31,43 +31,43 @@ public class StringUtilTest {
     public void zeroPadIntWithMoreDigitsTest() {
         String paddedString = StringUtil.zeroPadInt(500,5);
 
-        Assert.assertEquals (paddedString.length(), 5);
-        Assert.assertEquals (paddedString,("00500"));
+        Assert.assertEquals (5,paddedString.length());
+        Assert.assertEquals ("00500",paddedString);
     }
 
     @Test
     public void findAndReplaceLastCharTest() {
         String modifiedString = StringUtil.findAndReplace("ABCDEFG","G","H");
 
-        Assert.assertEquals (modifiedString,("ABCDEFH"));
+        Assert.assertEquals ("ABCDEFH",modifiedString);
     }
 
     @Test
     public void findAndReplaceEntireStringWithSameTest() {
         String modifiedString = StringUtil.findAndReplace("ABCDEFG","ABCDEFG","ABCDEFG");
 
-        Assert.assertEquals (modifiedString,("ABCDEFG"));
+        Assert.assertEquals ("ABCDEFG",modifiedString);
     }
 
     @Test
     public void findAndReplaceEntireStringWithEmptyTest() {
         String modifiedString = StringUtil.findAndReplace("ABCDEFG","ABCDEFG","");
 
-        Assert.assertEquals (modifiedString,(""));
+        Assert.assertEquals ("",modifiedString);
     }
 
     @Test
     public void deleteCharactersInStringWithNothingTest() {
         String modifiedString = StringUtil.deleteCharacters("ABCDEFG","");
 
-        Assert.assertEquals (modifiedString,("ABCDEFG"));
+        Assert.assertEquals ("ABCDEFG",modifiedString);
     }
 
     @Test
     public void deleteCharactersInStringWithEverythingTest() {
         String modifiedString = StringUtil.deleteCharacters("ABCDEFG","ABCDEFG");
 
-        Assert.assertEquals (modifiedString,(""));
+        Assert.assertEquals ("",modifiedString);
     }
 
 
@@ -75,6 +75,6 @@ public class StringUtilTest {
     public void deleteCharactersInStringWithRepeatedCharctersTest() {
         String modifiedString = StringUtil.deleteCharacters("ABCDEFG","AABB");
 
-        Assert.assertEquals (modifiedString,("CDEFG"));
+        Assert.assertEquals ("CDEFG",modifiedString);
     }
 }
